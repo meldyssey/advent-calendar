7e770c981e7a0f4aa6e6acf3eb674610ca46543d
meldyssey <meldyssey15@gmail.com>
2025-12-01 11:23:44 +0900

    fix: 개발 로컬환경에서 로그인되도록 팝업으로 수정

--- src/firebase/auth.ts
+++ src/firebase/auth.ts
@@ -1,26 +1,22 @@
 import { auth } from "./config";
-import {
-  signInWithRedirect,
-  GoogleAuthProvider,
+import { 
+  signInWithPopup, 
+  GoogleAuthProvider ,
   setPersistence,
   browserSessionPersistence,
+  type User,
 } from "firebase/auth";
 
-export const signInWithGoogle = async (): Promise<void> => {
+export const signInWithGoogle = async (): Promise<User> => {
   const provider = new GoogleAuthProvider();
   try {
-    console.log('🔍 현재 도메인:', window.location.hostname);
-    console.log('🔍 현재 URL:', window.location.href);
-    console.log('🔍 Auth Config:', auth.config);
-
     await setPersistence(auth, browserSessionPersistence);
-    await signInWithRedirect(auth, provider);
-  } catch (error: unknown) {
-    console.error('로그인 실패', error);
-    if (error && typeof error === 'object' && 'code' in error) {
-      console.error('❌ 에러 코드:', error.code);
-      console.error('❌ 에러 메시지:', 'message' in error ? error.message : 'Unknown error');
-    }
+
+    const result = await signInWithPopup(auth, provider);
+    console.log('로그인 성공', result.user);
+    return result.user;
+  } catch (error) {
+    console.error('로그인 실패', error)
     throw error;
   }
 }
